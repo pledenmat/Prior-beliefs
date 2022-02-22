bc2b448dd57f3bd6d92d097b3b803c04e48b3036
Pierre Le Denmat <pledenmat1@gmail.com>
2022-02-22 10:20:14 +0100

    Correct color accuracy RT

--- script/model_paper.R
+++ script/model_paper.R
@@ -535,8 +535,8 @@ mtext("Confidence",2,at=.7,line=2.5,cex=1.75);
 axis(1,at=0:(n-1),labels=names(x), cex.axis=1.5);
 mtext("Trial difficulty",1,3,at=1,cex=1.75)
 means <- sapply(x, mean);n<- length(x)
-legend("top",legend=c("Difficult","Medium","Easy"),
-       title = "Training condition",pch=rep(16,3),bty = "n",inset=0, 
+legend("top",legend=c("Difficult","Medium","Easy"),title = "Training condition",
+       pch=rep(16,3),bty = "n",inset=0, 
        cex = 1.5,col=c("brown3","cyan4","darkgoldenrod3"), horiz = T)
 for(i in seq(.5,.9,length.out = 5)) abline(h=i,col="lightgrey",lty = "dashed")
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="brown3",lwd=lwddat,lty = "dashed")
@@ -679,25 +679,29 @@ x_sim = cast(snrcorlowSim,sub~coh,value='cor');xmed_sim = cast(snrcormedSim,sub~
 x <- x[,c("hard","average","easy")];xmed <- xmed[,c("hard","average","easy")];x_sim <- x_sim[,c("hard","average","easy")];xmed_sim <- xmed_sim[,c("hard","average","easy")];
 xhigh_sim <- xhigh_sim[,c("hard","average","easy")];xhigh <- xhigh[,c("hard","average","easy")]
 
-par(mfrow=c(1,1))
-stripchart(x, ylim=c(0.6,1), xlim=c(-.05,n-1), vertical = TRUE, col="white",frame=F,xaxt='n',
-           main="",cex.axis=1.25)
-mtext("Accuracy",2,at=.8,line=3,cex=1.75);axis(1,at=0:(n-1),labels=names(x), cex.axis=1.5);mtext("Trial difficulty",1,3,at=1,cex=1.75)
-means <- sapply(x, mean);n<- length(x)
-# lines(0:(n-1),colMeans(x_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(1,0,0,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(x_sim,na.rm=T) + (colSds(as.matrix(x_sim))/sqrt(N1)),
-                             (colMeans(x_sim,na.rm=T) - colSds(as.matrix(x_sim))/sqrt(N1))[3:1]),
+stripchart(x, ylim=c(0.6,1.1), xlim=c(-.05,n-1), vertical = TRUE, col="white",
+           frame=F,xaxt='n',main=NULL,yaxt='n')
+axis(1,at=0:(n-1),labels=names(x), cex.axis=1.5);
+axis(2,seq(0.6,1,.1),cex.axis=1.5)
+mtext("Accuracy",2,at=.8,line=2.5,cex=1.75);
+mtext("Trial difficulty",1,3,at=1,cex=1.75)
+for(i in seq(.6,1,.1)) abline(h=i,col="lightgrey",lty = "dashed")
+legend("top",legend=c("Negative","Average","Positive"),title = "Feedback condition",
+       pch=rep(16,3),bty = "n",inset=0, cex = 1.5,col=c("brown3","cyan4","darkgoldenrod3"),
+       horiz = T)
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(x_sim,na.rm=T) + (colSds(as.matrix(x_sim))/sqrt(N1)),
+          (colMeans(x_sim,na.rm=T) - colSds(as.matrix(x_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(1,0,0,.2))
-# lines(0:(n-1),colMeans(xmed_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(1,.5,0,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(xmed_sim,na.rm=T) + (colSds(as.matrix(xmed_sim),na.rm=T)/sqrt(N1)),
-                             (colMeans(xmed_sim,na.rm=T) - colSds(as.matrix(xmed_sim),na.rm=T)/sqrt(N1))[3:1]),
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(xmed_sim,na.rm=T) + (colSds(as.matrix(xmed_sim))/sqrt(N1)),
+          (colMeans(xmed_sim,na.rm=T) - colSds(as.matrix(xmed_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(1,.5,0,.2))
-# lines(0:(n-1),colMeans(xhigh_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(0,0,1,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(xhigh_sim,na.rm=T) + (colSds(as.matrix(xhigh_sim),na.rm=T)/sqrt(N1)),
-                             (colMeans(xhigh_sim,na.rm=T) - colSds(as.matrix(xhigh_sim),na.rm=T)/sqrt(N1))[3:1]),
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(xhigh_sim,na.rm=T) + (colSds(as.matrix(xhigh_sim))/sqrt(N1)),
+          (colMeans(xhigh_sim,na.rm=T) - colSds(as.matrix(xhigh_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(0,0,1,.2))
-legend(.05,1,legend=c("Negative","Average","Positive"),title = "Feedback condition",
-       pch=rep(16,3),bty = "n",inset=.1, cex = 1.5,col=c("brown3","cyan4","darkgoldenrod3"))
+means <- sapply(x, mean);n<- length(x)
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="brown3",lwd=lwddat)
 error.bar(0:(n-1),means,colSds(as.matrix(x),na.rm=T)/sqrt(N1),lwd=lwdgr,col="brown3")
 means <- sapply(xmed, mean,na.rm=T)
@@ -706,6 +710,7 @@ error.bar(0:(n-1),means,colSds(as.matrix(xmed),na.rm=T)/sqrt(N1),lwd=lwdgr,col="
 means <- sapply(xhigh, mean,na.rm=T)
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="darkgoldenrod3",lwd=lwddat)
 error.bar(0:(n-1),means,colSds(as.matrix(xhigh),na.rm=T)/sqrt(N1),lwd=lwdgr,col="darkgoldenrod3")
+
 
 ## Experiment 2
 #Aggregate conf for Data2
@@ -738,40 +743,50 @@ x_sim = cast(snrcorlowSim,sub~coh,value='cor');xmed_sim = cast(snrcormedSim,sub~
 x <- x[,c("hard","average","easy")];xmed <- xmed[,c("hard","average","easy")];x_sim <- x_sim[,c("hard","average","easy")];xmed_sim <- xmed_sim[,c("hard","average","easy")];
 xhigh_sim <- xhigh_sim[,c("hard","average","easy")];xhigh <- xhigh[,c("hard","average","easy")]
 
-stripchart(x, ylim=c(.6,1), xlim=c(-.05,n-1), vertical = TRUE, col="white",frame=F,xaxt='n',
-           main="",cex.axis=1.25)
-mtext("Accuracy",2,at=.8,line=3,cex=1.75);axis(1,at=0:(n-1),labels=names(x), cex.axis=1.5);mtext("Trial difficulty",1,3,at=1,cex=1.75)
-means <- sapply(x, mean);n<- length(x)
-# lines(0:(n-1),colMeans(x_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(1,0,0,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(x_sim,na.rm=T) + (colSds(as.matrix(x_sim))/sqrt(Nsub_2)),(colMeans(x_sim,na.rm=T) - colSds(as.matrix(x_sim))/sqrt(Nsub_2))[3:1]),
+stripchart(x, ylim=c(0.6,1.1), xlim=c(-.05,n-1), vertical = TRUE, col="white",
+           frame=F,xaxt='n',main=NULL,yaxt='n')
+axis(1,at=0:(n-1),labels=names(x), cex.axis=1.5);
+axis(2,seq(0.6,1,.1),cex.axis=1.5)
+mtext("Accuracy",2,at=.8,line=2.5,cex=1.75);
+mtext("Trial difficulty",1,3,at=1,cex=1.75)
+for(i in seq(.6,1,.1)) abline(h=i,col="lightgrey",lty = "dashed")
+legend("top",legend=c("Difficult","Medium","Easy"),title = "Training condition",
+       pch=rep(16,3),bty = "n",inset=0, cex = 1.5,col=c("brown3","cyan4","darkgoldenrod3"),
+       horiz = T)
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(x_sim,na.rm=T) + (colSds(as.matrix(x_sim))/sqrt(N1)),
+          (colMeans(x_sim,na.rm=T) - colSds(as.matrix(x_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(1,0,0,.2))
-# lines(0:(n-1),colMeans(xmed_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(1,.5,0,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(xmed_sim,na.rm=T) + (colSds(as.matrix(xmed_sim),na.rm=T)/sqrt(Nsub_2)),
-                             (colMeans(xmed_sim,na.rm=T) - colSds(as.matrix(xmed_sim),na.rm=T)/sqrt(Nsub_2))[3:1]),
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(xmed_sim,na.rm=T) + (colSds(as.matrix(xmed_sim))/sqrt(N1)),
+          (colMeans(xmed_sim,na.rm=T) - colSds(as.matrix(xmed_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(1,.5,0,.2))
-# lines(0:(n-1),colMeans(xhigh_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(0,0,1,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(xhigh_sim,na.rm=T) + (colSds(as.matrix(xhigh_sim),na.rm=T)/sqrt(Nsub_2)),(colMeans(xhigh_sim,na.rm=T) - colSds(as.matrix(xhigh_sim),na.rm=T)/sqrt(Nsub_2))[3:1]),
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(xhigh_sim,na.rm=T) + (colSds(as.matrix(xhigh_sim))/sqrt(N1)),
+          (colMeans(xhigh_sim,na.rm=T) - colSds(as.matrix(xhigh_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(0,0,1,.2))
-legend(.05,1,legend=c("Hard","Average","Easy"),title = "Training condition",
-       pch=rep(16,3),bty = "n",inset=.1, cex = 1.5,col=c("brown3","cyan4","darkgoldenrod3"))
+means <- sapply(x, mean);n<- length(x)
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="brown3",lwd=lwddat)
-error.bar(0:(n-1),means,colSds(as.matrix(x),na.rm=T)/sqrt(Nsub_2),lwd=lwdgr,col="brown3")
+error.bar(0:(n-1),means,colSds(as.matrix(x),na.rm=T)/sqrt(N1),lwd=lwdgr,col="brown3")
 means <- sapply(xmed, mean,na.rm=T)
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="cyan4",lwd=lwddat)
-error.bar(0:(n-1),means,colSds(as.matrix(xmed),na.rm=T)/sqrt(Nsub_2),lwd=lwdgr,col="cyan4")
+error.bar(0:(n-1),means,colSds(as.matrix(xmed),na.rm=T)/sqrt(N1),lwd=lwdgr,col="cyan4")
 means <- sapply(xhigh, mean,na.rm=T)
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="darkgoldenrod3",lwd=lwddat)
-error.bar(0:(n-1),means,colSds(as.matrix(xhigh),na.rm=T)/sqrt(Nsub_2),lwd=lwdgr,col="darkgoldenrod3")
+error.bar(0:(n-1),means,colSds(as.matrix(xhigh),na.rm=T)/sqrt(N1),lwd=lwdgr,col="darkgoldenrod3")
 
 
 # Plot overlay RT ---------------------------------------------------------
 ## Experiment 1
 #Aggregate conf for data
-rtlow <- with(subset(Data1,selfconf=="lowSC"),aggregate(rt,by=list(sub,coh),mean));names(rtlow) <- c('sub','coh','rt')
+rtlow <- with(subset(Data1,selfconf=="lowSC"),aggregate(rt,by=list(sub,coh),mean));
+names(rtlow) <- c('sub','coh','rt')
 rtlow <- cast(rtlow,sub~coh,)
-rtmed <- with(subset(Data1,selfconf=="mediumSC"),aggregate(rt,by=list(sub,coh),mean));names(rtmed) <- c('sub','coh','rt')
+rtmed <- with(subset(Data1,selfconf=="mediumSC"),aggregate(rt,by=list(sub,coh),mean));
+names(rtmed) <- c('sub','coh','rt')
 rtmed <- cast(rtmed,sub~coh)
-rthigh <- with(subset(Data1,selfconf=="highSC"),aggregate(rt,by=list(sub,coh),mean));names(rthigh) <- c('sub','coh','rt')
+rthigh <- with(subset(Data1,selfconf=="highSC"),aggregate(rt,by=list(sub,coh),mean));
+names(rthigh) <- c('sub','coh','rt')
 rthigh <- cast(rthigh,sub~coh)
 
 
@@ -799,22 +814,31 @@ x_sim = cast(snrrtlowSim,sub~coh,value='rt');xmed_sim = cast(snrrtmedSim,sub~coh
 x <- x[,c("hard","average","easy")];xmed <- xmed[,c("hard","average","easy")];x_sim <- x_sim[,c("hard","average","easy")];xmed_sim <- xmed_sim[,c("hard","average","easy")];
 xhigh_sim <- xhigh_sim[,c("hard","average","easy")];xhigh <- xhigh[,c("hard","average","easy")]
 
-par(mfrow=c(1,1))
-stripchart(x, ylim=c(0.6,1.1), xlim=c(-.05,n-1), vertical = TRUE, col="white",frame=F,xaxt='n',
-           main="",cex.axis=1.25)
-mtext("RT (s)",2,at=.85,line=2.5,cex=1.75);axis(1,at=0:(n-1),labels=names(x), cex.axis=1.5);mtext("Trial difficulty",1,3,at=1,cex=1.75)
-means <- sapply(x, mean);n<- length(x)
-# lines(0:(n-1),colMeans(x_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(1,0,0,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(x_sim,na.rm=T) + (colSds(as.matrix(x_sim))/sqrt(N1)),(colMeans(x_sim,na.rm=T) - colSds(as.matrix(x_sim))/sqrt(N1))[3:1]),
+
+stripchart(x, ylim=c(0.6,1.2), xlim=c(-.05,n-1), vertical = TRUE, col="white",
+           frame=F,xaxt='n',main=NULL,yaxt='n')
+axis(1,at=0:(n-1),labels=names(x), cex.axis=1.5);
+axis(2,seq(0.6,1.1,.1),cex.axis=1.5)
+mtext("RT (s)",2,at=.85,line=2.5,cex=1.75);
+mtext("Trial difficulty",1,3,at=1,cex=1.75)
+for(i in seq(.6,1.1,.1)) abline(h=i,col="lightgrey",lty = "dashed")
+legend("top",legend=c("Negative","Average","Positive"),title = "Feedback condition",
+       pch=rep(16,3),bty = "n",inset=0, cex = 1.5,col=c("brown3","cyan4","darkgoldenrod3"),
+       horiz = T)
+
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(x_sim,na.rm=T) + (colSds(as.matrix(x_sim))/sqrt(N1)),
+          (colMeans(x_sim,na.rm=T) - colSds(as.matrix(x_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(1,0,0,.2))
-# lines(0:(n-1),colMeans(xmed_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(1,.5,0,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(xmed_sim,na.rm=T) + (colSds(as.matrix(xmed_sim),na.rm=T)/sqrt(N1)),(colMeans(xmed_sim,na.rm=T) - colSds(as.matrix(xmed_sim),na.rm=T)/sqrt(N1))[3:1]),
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(xmed_sim,na.rm=T) + (colSds(as.matrix(xmed_sim))/sqrt(N1)),
+          (colMeans(xmed_sim,na.rm=T) - colSds(as.matrix(xmed_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(1,.5,0,.2))
-# lines(0:(n-1),colMeans(xhigh_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(0,0,1,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(xhigh_sim,na.rm=T) + (colSds(as.matrix(xhigh_sim),na.rm=T)/sqrt(N1)),(colMeans(xhigh_sim,na.rm=T) - colSds(as.matrix(xhigh_sim),na.rm=T)/sqrt(N1))[3:1]),
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(xhigh_sim,na.rm=T) + (colSds(as.matrix(xhigh_sim))/sqrt(N1)),
+          (colMeans(xhigh_sim,na.rm=T) - colSds(as.matrix(xhigh_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(0,0,1,.2))
-legend(.05,1.1,legend=c("Negative","Average","Positive"),title = "Feedback condition",
-       pch=rep(16,3),bty = "n",inset=.1, cex = 1.5,col=c("brown3","cyan4","darkgoldenrod3"))
+means <- sapply(x, mean);n<- length(x)
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="brown3",lwd=lwddat)
 error.bar(0:(n-1),means,colSds(as.matrix(x),na.rm=T)/sqrt(N1),lwd=lwdgr,col="brown3")
 means <- sapply(xmed, mean,na.rm=T)
@@ -855,29 +879,37 @@ x_sim = cast(snrrtlowSim,sub~coh,value='rt');xmed_sim = cast(snrrtmedSim,sub~coh
 x <- x[,c("hard","average","easy")];xmed <- xmed[,c("hard","average","easy")];x_sim <- x_sim[,c("hard","average","easy")];xmed_sim <- xmed_sim[,c("hard","average","easy")];
 xhigh_sim <- xhigh_sim[,c("hard","average","easy")];xhigh <- xhigh[,c("hard","average","easy")]
 
-stripchart(x, ylim=c(.6,1.1), xlim=c(-.05,n-1), vertical = TRUE, col="white",frame=F,xaxt='n',
-           main="",cex.axis=1.25)
-mtext("RT (s)",2,at=.85,line=2.5,cex=1.75);axis(1,at=0:(n-1),labels=names(x), cex.axis=1.5);mtext("Trial difficulty",1,3,at=1,cex=1.75)
-means <- sapply(x, mean);n<- length(x)
-# lines(0:(n-1),colMeans(x_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(1,0,0,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(x_sim,na.rm=T) + (colSds(as.matrix(x_sim))/sqrt(Nsub_2)),(colMeans(x_sim,na.rm=T) - colSds(as.matrix(x_sim))/sqrt(Nsub_2))[3:1]),
+stripchart(x, ylim=c(0.6,1.2), xlim=c(-.05,n-1), vertical = TRUE, col="white",
+           frame=F,xaxt='n',main=NULL,yaxt='n')
+axis(1,at=0:(n-1),labels=names(x), cex.axis=1.5);
+axis(2,seq(0.6,1.1,.1),cex.axis=1.5)
+mtext("RT (s)",2,at=.85,line=2.5,cex=1.75);
+mtext("Trial difficulty",1,3,at=1,cex=1.75)
+for(i in seq(.6,1.1,.1)) abline(h=i,col="lightgrey",lty = "dashed")
+legend("top",legend=c("Difficult","Medium","Easy"),title = "Training condition",
+       pch=rep(16,3),bty = "n",inset=0, cex = 1.5,col=c("brown3","cyan4","darkgoldenrod3"),
+       horiz = T)
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(x_sim,na.rm=T) + (colSds(as.matrix(x_sim))/sqrt(N1)),
+          (colMeans(x_sim,na.rm=T) - colSds(as.matrix(x_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(1,0,0,.2))
-# lines(0:(n-1),colMeans(xmed_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(1,.5,0,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(xmed_sim,na.rm=T) + (colSds(as.matrix(xmed_sim),na.rm=T)/sqrt(Nsub_2)),(colMeans(xmed_sim,na.rm=T) - colSds(as.matrix(xmed_sim),na.rm=T)/sqrt(Nsub_2))[3:1]),
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(xmed_sim,na.rm=T) + (colSds(as.matrix(xmed_sim))/sqrt(N1)),
+          (colMeans(xmed_sim,na.rm=T) - colSds(as.matrix(xmed_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(1,.5,0,.2))
-# lines(0:(n-1),colMeans(xhigh_sim,na.rm=T),type='b',lty=2,cex=cexkl,lwd=lwdgr,pch=16,col=rgb(0,0,1,.5))
-polygon(c(0:(n-1),(n-1):0),c(colMeans(xhigh_sim,na.rm=T) + (colSds(as.matrix(xhigh_sim),na.rm=T)/sqrt(Nsub_2)),(colMeans(xhigh_sim,na.rm=T) - colSds(as.matrix(xhigh_sim),na.rm=T)/sqrt(Nsub_2))[3:1]),
+polygon(c(0:(n-1),(n-1):0),
+        c(colMeans(xhigh_sim,na.rm=T) + (colSds(as.matrix(xhigh_sim))/sqrt(N1)),
+          (colMeans(xhigh_sim,na.rm=T) - colSds(as.matrix(xhigh_sim))/sqrt(N1))[3:1]),
         border=F,col=rgb(0,0,1,.2))
-legend("bottomleft",legend=c("Hard","Average","Easy"),title = "Training condition",
-       pch=rep(16,3),bty = "n",inset=.1, cex = 1.5,col=c("brown3","cyan4","darkgoldenrod3"))
+means <- sapply(x, mean);n<- length(x)
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="brown3",lwd=lwddat)
-error.bar(0:(n-1),means,colSds(as.matrix(x),na.rm=T)/sqrt(Nsub_2),lwd=lwdgr,col="brown3")
+error.bar(0:(n-1),means,colSds(as.matrix(x),na.rm=T)/sqrt(N1),lwd=lwdgr,col="brown3")
 means <- sapply(xmed, mean,na.rm=T)
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="cyan4",lwd=lwddat)
-error.bar(0:(n-1),means,colSds(as.matrix(xmed),na.rm=T)/sqrt(Nsub_2),lwd=lwdgr,col="cyan4")
+error.bar(0:(n-1),means,colSds(as.matrix(xmed),na.rm=T)/sqrt(N1),lwd=lwdgr,col="cyan4")
 means <- sapply(xhigh, mean,na.rm=T)
 lines(0:(n-1),means,type='b',pch=16,cex=cexkl,col="darkgoldenrod3",lwd=lwddat)
-error.bar(0:(n-1),means,colSds(as.matrix(xhigh),na.rm=T)/sqrt(Nsub_2),lwd=lwdgr,col="darkgoldenrod3")
+error.bar(0:(n-1),means,colSds(as.matrix(xhigh),na.rm=T)/sqrt(N1),lwd=lwdgr,col="darkgoldenrod3")
 
 # OTHER PLOTS -------------------------------------------------------------,
 ## Correlation Objective/Subjective drift ====
